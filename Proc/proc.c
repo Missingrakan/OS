3eade03623bf84cb1828792e7710d51fe678c85a
星辰之洛 <3047451493@qq.com>
2020-10-19 23:12:53 +0800

    更新代码

--- Proc/proc.c
+++ Proc/proc.c
@@ -8,12 +8,17 @@ int main()
     char bar[102];
     memset(bar,'\0',sizeof(bar));
     const char*lable = "|/-\\";
+    int j=0;
+    int color[]={1,2,3,4,5,6,7};
     for(; i <= 100; i++)
     {
-        bar[i] = '*';
-        printf("[%-101s][%d%%][%c]\r",bar,i,lable[i%4]);
-        fflush(stdout);
-        usleep(100000);
+      bar[i] = '*';
+      printf("\033[3%dm[%-101s]\033[0m\033[33m[%d%%]\033[0m[%c]\r", color[j],bar,i,lable[i%4]);
+      fflush(stdout);
+      if(i%15 == 0){
+        ++j;
+      }
+      usleep(100000);
     }
     printf("\n");
     return 0;
